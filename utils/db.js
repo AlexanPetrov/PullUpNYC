0c352989733b49a4c8bdf4f49cc3e36441de8894
Alexander Petrovski <apfb11@gmail.com>
2024-03-07 16:48:01 -0500

    changed db connection to remote 2

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "sql5689454",
+  password: process.env.DB_PASSWORD || "BcaD87E6If",
+  database: process.env.DB_NAME || "sql5689454",
+  port: process.env.DB_PORT || 3306, // Ensure the port is also configurable
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
