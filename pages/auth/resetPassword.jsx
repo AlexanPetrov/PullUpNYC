ac82193dbb711157763fcb419925dc779e3c2bdf
Alexander Petrovski <apfb11@gmail.com>
2024-03-22 15:08:35 -0400

    adjust reset password

--- pages/auth/resetPassword.jsx
+++ pages/auth/resetPassword.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from '@/pages/auth/resetPassword.module.css';
-import ErrorModal from '@/components/ErrorModal'; 
+import ErrorModal from '@/components/ErrorModal';
 
 const ResetPassword = () => {
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
   const [message, setMessage] = useState('');
-  const [showErrorModal, setShowErrorModal] = useState(false); 
+  const [showErrorModal, setShowErrorModal] = useState(false);
   const router = useRouter();
 
   const { token } = router.query;
 
+  const validatePassword = (password) => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+\[\]{}|;:'",.<>\/\?])[A-Za-z\d!@#$%^&*()\-_=+\[\]{}|;:'",.<>\/\?]{8,}$/;
+    return regex.test(password);
+  };
+
+  const handlePasswordChange = (e) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    setPasswordError(!validatePassword(newPassword));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (password !== confirmPassword) {
-      setMessage("Passwords don't match.");
+
+    if (!validatePassword(password) || password !== confirmPassword) {
+      setMessage("Passwords don't match or don't meet the requirements.");
       setShowErrorModal(true);
       return;
     }
@@ -36,6 +49,7 @@ const ResetPassword = () => {
       }
     } catch (error) {
       setMessage(error.message);
+      setShowErrorModal(true);
     }
   };
 
@@ -50,9 +64,20 @@ const ResetPassword = () => {
           id="password"
           name="password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={handlePasswordChange}
           required
         />
+        {passwordError && (
+          <div className={styles.error}>
+            <ul>
+              <li>8 or more chars long</li>
+              <li>At least one uppercase letter</li>
+              <li>At least one lowercase letter</li>
+              <li>At least one number</li>
+              <li>{"At least one special char: !@#$%^&*()-_=+[]{}|;:'\",.<>/? "}</li>
+            </ul>
+          </div>
+        )}
         <label className={styles.cpassword} htmlFor="confirmPassword">Confirm New Password:</label>
         <input
           className={styles.inputfield}
@@ -63,7 +88,7 @@ const ResetPassword = () => {
           onChange={(e) => setConfirmPassword(e.target.value)}
           required
         />
-        <button type="submit" className={styles.button}>Reset Password</button>
+        <button type="submit" className={styles.button} disabled={passwordError}>Reset Password</button>
       </form>
       {showErrorModal && <ErrorModal message={message} onClose={() => setShowErrorModal(false)} />}
       {!showErrorModal && message && <p className={styles.message}>{message}</p>}
